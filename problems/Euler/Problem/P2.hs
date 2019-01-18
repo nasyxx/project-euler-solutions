47d8c597256874024615957a12af289e14e08eac
Nasy <nasyxx+git@gmail.com>
2019-01-18 11:07:33 +0800

    Add thought of Problem 2.

---
+++ problems/Euler/Problem/P2.hs
@@ -0,0 +1,57 @@
+{-
+ Excited without bugs, have fun ("▔□▔)/hi~♡ Nasy.
+ ------------------------------------------------
+ |             *         *
+ |                  .                .
+ |           .
+ |     *                      ,
+ |                   .
+ |
+ |                               *
+ |          |\___/|
+ |          )    -(             .              ·
+ |         =\ -   /=
+ |           )===(       *
+ |          /   - \
+ |          |-    |
+ |         /   -   \     0.|.0
+ |  NASY___\__( (__/_____(\=/)__+1s____________
+ |  ______|____) )______|______|______|______|_
+ |  ___|______( (____|______|______|______|____
+ |  ______|____\_|______|______|______|______|_
+ |  ___|______|______|______|______|______|____
+ |  ______|______|______|______|______|______|_
+ |  ___|______|______|______|______|______|____
+
+There are more things in heaven and earth, Horatio, than are dreamt.
+   -- From "Hamlet"
+--------------------------------------------------------------------------------
+
+-}
+
+--------------------------------------------------------------------------------
+-- |
+-- Filename   : P2.hs
+-- Project    : nasy-euler
+-- Author     : Nasy
+-- License    : LGPL-3.0
+--
+-- Maintainer : Nasy <nasyxx+euler@gmail.com>
+--
+-- Nasy's Haskell Solutions of Project Euler.
+--
+-- https://github.com/nasyxx/project-euler-solutions
+-- https://projecteuler.net/problem=2
+--
+-- Exhaust the fib numbers.
+--
+--------------------------------------------------------------------------------
+
+module Euler.Problem.P2 where
+
+
+fib :: Num t => t -> t -> [t]
+fib a b = a : fib b (a + b)
+
+ans :: Integer
+ans = sum . filter even . takeWhile (< 4000000) $ fib 1 2
