7be07a73ffefd6c96b500ae9e3bbae2d6f814004
Nasy <nasyxx+git@gmail.com>
2019-01-19 09:33:59 +0800

    Finished Problem 5.

--- problems/Euler/Problem/P5.hs
+++ problems/Euler/Problem/P5.hs
@@ -31,7 +31,7 @@ There are more things in heaven and earth, Horatio, than are dreamt.
 
 --------------------------------------------------------------------------------
 -- |
--- Filename   : Problem.hs
+-- Filename   : P5.hs
 -- Project    : nasy-euler
 -- Author     : Nasy
 -- License    : LGPL-3.0
@@ -41,18 +41,18 @@ There are more things in heaven and earth, Horatio, than are dreamt.
 -- Nasy's Haskell Solutions of Project Euler.
 --
 -- https://github.com/nasyxx/project-euler-solutions
+-- https://projecteuler.net/problem=5
+--
+-- The problem is find the least common multiple of 1 to 20.
+--
+-- Thus, we can lcm 1 (lcm 2 ( lcm 3 ... (lcm 9 10)))
 --
 --------------------------------------------------------------------------------
 
-module Euler.Problem where
-import qualified Euler.Problem.P1              as P1
-import qualified Euler.Problem.P2              as P2
-import qualified Euler.Problem.P3              as P3
-import qualified Euler.Problem.P4              as P4
-import qualified Euler.Problem.P5              as P5
+module Euler.Problem.P5 where
 
-answers :: [Integer]
-answers = [P1.ans, P2.ans, P3.ans, P4.ans, P5.ans]
+lcmN :: Integral a => a -> a
+lcmN n = foldl1 lcm [2 .. n]
 
-counts :: Int
-counts = 5
+ans :: Integer
+ans = lcmN 20
