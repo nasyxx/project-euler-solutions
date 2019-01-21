6b47610bda1e01603fa531165e2cf070884de3a4
Nasy <nasyxx+git@gmail.com>
2019-01-21 22:12:16 +0800

    Finished Problem 7.

--- problems/Euler/Problem/P7.hs
+++ problems/Euler/Problem/P7.hs
@@ -31,7 +31,7 @@ There are more things in heaven and earth, Horatio, than are dreamt.
 
 --------------------------------------------------------------------------------
 -- |
--- Filename   : Problem.hs
+-- Filename   : P7.hs
 -- Project    : nasy-euler
 -- Author     : Nasy
 -- License    : LGPL-3.0
@@ -41,39 +41,26 @@ There are more things in heaven and earth, Horatio, than are dreamt.
 -- Nasy's Haskell Solutions of Project Euler.
 --
 -- https://github.com/nasyxx/project-euler-solutions
+-- https://projecteuler.net/problem=7
 --
+-- Generate primes.
+--
+-- The prime in the problem is not really big, we can use a simple
+-- sieve but a really complex one.
+--
+-- BTW, there is an interesting paper here:
+-- https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
 --------------------------------------------------------------------------------
+module Euler.Problem.P7 where
 
-module Euler.Problem where
-import qualified Euler.Problem.P1              as P1
-import qualified Euler.Problem.P2              as P2
-import qualified Euler.Problem.P3              as P3
-import qualified Euler.Problem.P4              as P4
-import qualified Euler.Problem.P5              as P5
-import qualified Euler.Problem.P6              as P6
-import qualified Euler.Problem.P7              as P7
-
-data Answer = I Int Integer | F Int Float
-
-instance Show Answer where
-    show (I p n) = "Problem " ++ show p ++ ": " ++ show n
-    show (F p n) = "Problem " ++ show p ++ ": " ++ show n
-
-answers :: [Answer]
-answers = zipWith
-    set
-    [1 ..]
-    [ Left P1.ans
-    , Left P2.ans
-    , Left P3.ans
-    , Left P4.ans
-    , Left P5.ans
-    , Left $ truncate P6.ans
-    , Left P7.ans
-    ]
+primes :: [Integer]
+primes = 2 : sieve [3, 5 ..]
   where
-    set idx (Left  n) = I idx n
-    set idx (Right n) = F idx n
+    modulus n =
+        map (mod n)
+            . takeWhile (<= (floor . sqrt $ (fromIntegral n :: Float)))
+            $ primes
+    sieve = filter (all (> 0) . modulus)
 
-counts :: Int
-counts = length answers
+ans :: Integer
+ans = primes !! 10000
