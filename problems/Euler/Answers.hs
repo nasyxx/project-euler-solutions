7770d357c0a0bdf9e43f55ea116ce68b9b46b7a4
Nasy <nasyxx+git@gmail.com>
2019-02-13 08:50:14 +0800

    Finished Problem 20.

--- problems/Euler/Answers.hs
+++ problems/Euler/Answers.hs
@@ -70,6 +70,7 @@ import qualified Euler.Problem.P16             as P16
 import qualified Euler.Problem.P17             as P17
 import qualified Euler.Problem.P18             as P18
 import qualified Euler.Problem.P19             as P19
+import qualified Euler.Problem.P20             as P20
 
 data Answer = I Int Integer | Io Int (IO Integer) | Ios Int (IO String)
 
@@ -110,4 +111,5 @@ answers = zipWith
     , Left P17.ans
     , Right $ Left P18.ans
     , Left P19.ans
+    , Left P20.ans
     ]
---
+++ problems/Euler/Problem/P20.hs
@@ -0,0 +1,55 @@
+{-
+ Excited without bugs, have fun ("▔□▔)/hi~♡ Nasy.
+ ------------------------------------------------
+ |             *         *
+ |                  .                .
+ |           .
+ |     *                      ,
+ |                   .
+ |
+ |                               *
+ |          |\___/|
+ |          )    -(             .              ·
+ |         =\ -   /=
+ |           )===(       *
+ |          /   - \
+ |          |-    |
+ |         /   -   \     0.|.0
+ |  NASY___\__( (__/_____(\=/)__+1s____________
+ |  ______|____) )______|______|______|______|_
+ |  ___|______( (____|______|______|______|____
+ |  ______|____\_|______|______|______|______|_
+ |  ___|______|______|______|______|______|____
+ |  ______|______|______|______|______|______|_
+ |  ___|______|______|______|______|______|____
+
+There are more things in heaven and earth, Horatio, than are dreamt.
+   -- From "Hamlet"
+--------------------------------------------------------------------------------
+
+-}
+
+--------------------------------------------------------------------------------
+-- |
+-- Filename   : P20.hs
+-- Project    : nasy-euler
+-- Author     : Nasy
+-- License    : GPL-3.0+
+--
+-- Maintainer : Nasy <nasyxx+euler@gmail.com>
+--
+-- Nasy's Haskell Solutions of Project Euler.
+--
+-- https://github.com/nasyxx/project-euler-solutions
+-- https://projecteuler.net/problem=20
+--
+-- Find the sum of the digits in the number 100!.
+--
+-- Calculate ans straightforwardly.
+--------------------------------------------------------------------------------
+
+module Euler.Problem.P20 where
+import           Data.Char                      ( digitToInt )
+
+ans :: Integer
+ans = toInteger . sum . map digitToInt $ show (product [1 .. 100] :: Integer)
